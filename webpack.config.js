0d2306854261a119f86c01aae9967c50a5fa29a3
Adnan Afsari <aarahul1991@gmail.com>
2019-07-12 09:42:17 +0600

    bubble added

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,20 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 };
